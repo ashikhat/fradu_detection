6028d59dd5be111da0cd53f152b8955ba4a2dffa
ashikhat <ashish.khatakalle@gmail.com>
2023-03-27 16:53:42 +0530

    added  ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print ('creation of model in new branch')
+
+def model1():
+    print ('adding code for model 1')
+
+def model2():
+    print ('adding code for model 2')
+
+def model3():
+    print ('adding code for model 3')
